214c437f15397557f87f535a06fa1691dd8badd7
AsukaFukaumi <asuarks@outlook.com>
2020-06-07 01:43:15 +0900

    postsテーブル、usersテーブル作成
    postsテーブルとusersテーブルを作成し、rails db:migrate

--- db/migrate/20200606150120_create_posts.rb
+++ db/migrate/20200606150120_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
+      t.string :title
+      t.text :file
+      t.text :detail
+      t.integer :user_id
       t.timestamps
     end
   end
