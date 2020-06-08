367f62792c573e3fb5441c87449dd94801a08eaf
AsukaFukaumi <asuarks@outlook.com>
2020-06-08 16:12:34 +0900

    posts show設定
    ルーティング設定
    showアクション設定
    indexに詳細ボタン追加
    show.html.erb 作成
    
    https://master.tech-camp.in/curriculums/3633#3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
-  resources :posts, only: [:index, :new, :create, :destroy, :edit, :update]
+  resources :posts, only: [:index, :new, :create, :destroy, :edit, :update, :show]
 end
