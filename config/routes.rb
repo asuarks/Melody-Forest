8a52efcc30bc38a1158f4fcc4fd9d6b87da9c98d
AsukaFukaumi <asuarks@outlook.com>
2020-06-08 19:33:49 +0900

    users show設定
    マイページのルーティングを設定
    マイページへのリンクを設定
    https://master.tech-camp.in/curriculums/3646#8

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
   resources :posts, only: [:index, :new, :create, :destroy, :edit, :update, :show]
+  resources :users, only: :show
 end
