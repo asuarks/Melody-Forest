7fef05af1961e6d7d0ce94ea859ddce923bd9037
AsukaFukaumi <asuarks@outlook.com>
2020-06-08 22:19:06 +0900

    コメントルーティング設定
    コメントのルーティングをネストさせて追加
    Topへ戻るリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
   resources :posts, only: [:index, :new, :create, :destroy, :edit, :update, :show]
+  resources :posts do
+    resources :comments, only: :create
+  end
   resources :users, only: :show
 end
