9cf347961f87f9c79ea870cde98dc0b56179def5
AsukaFukaumi <asuarks@outlook.com>
2020-06-12 16:40:41 +0900

    新規登録・ログインフォーム色変更
    色変更と微調整

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -1,13 +1,13 @@
 window.onload = function(){
 
 $('#signup').click(function() {
-  $('.pinkbox').css('transform', 'translateX(80%)');
+  $('.greenbox').css('transform', 'translateX(80%)');
   $('.signin').addClass('nodisplay');
   $('.signup').removeClass('nodisplay');
 });
 
 $('#signin').click(function() {
-  $('.pinkbox').css('transform', 'translateX(0%)');
+  $('.greenbox').css('transform', 'translateX(0%)');
   $('.signup').addClass('nodisplay');
   $('.signin').removeClass('nodisplay');
 });
