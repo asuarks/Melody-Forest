cb283bc34e9f0d475c9e109d42a376e002f1d090
AsukaFukaumi <asuarks@outlook.com>
2020-06-15 17:27:05 +0900

    ユーザー編集機能の作成
    User モデル用の Devise controller を作成
    bundle exec rails g devise:controllers users
    
    Profile モデルの定義
    
    User モデルと Profile モデルの 1対1関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   has_many :posts
   has_many :comments
+
+  has_one :profile
 end
---
+++ db/migrate/20200615081600_create_profiles.rb
@@ -0,0 +1,12 @@
+class CreateProfiles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :profiles do |t|
+      t.integer :user_id
+      t.string :name
+      t.string :avatar
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
