64a9a06727fa1130fb5040e9be8650f1bb4617fb
asuarks <asuarks@outlook.com>
2020-07-24 23:44:02 +0900

    リロードしないとjavascriptが動かないバグの修正

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -1,15 +1,15 @@
-window.addEventListener('DOMContentLoaded', function(){
+document.addEventListener("turbolinks:load", function() {
 
-$('#signup').click(function() {
-  $('.greenbox').css('transform', 'translateX(80%)');
-  $('.signin').addClass('nodisplay');
-  $('.signup').removeClass('nodisplay');
-});
+  $('#signup').click(function() {
+    $('.greenbox').css('transform', 'translateX(80%)');
+    $('.signin').addClass('nodisplay');
+    $('.signup').removeClass('nodisplay');
+  });
 
-$('#signin').click(function() {
-  $('.greenbox').css('transform', 'translateX(0%)');
-  $('.signup').addClass('nodisplay');
-  $('.signin').removeClass('nodisplay');
-});
+  $('#signin').click(function() {
+    $('.greenbox').css('transform', 'translateX(0%)');
+    $('.signup').addClass('nodisplay');
+    $('.signin').removeClass('nodisplay');
+  });
 
-});
+})
